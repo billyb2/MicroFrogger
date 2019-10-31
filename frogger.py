4a1bd0657a5d2ababf9598d1e280df01b5f58bf6
William Batista <45850508+bootlegbilly@users.noreply.github.com>
2019-10-31 14:52:05 -0400

    Got basic controls working
    Works with tilt and pressing A and B, just gotta get y movement down.

--- frogger.py
+++ frogger.py
@@ -1,22 +1,51 @@
 # Add your Python code here. E.g.
 from microbit import *
 
+#gotta set variables outside of loop
+x = 0;
+y = 0;
 
+clear = Image(  "00000:"
+                "00000:"
+                "00000:"
+                "00000:"
+                "00000")
+                
+                
+#run on a forever loop
 while True:
+    
+    #how far the microbit is leaning left or right
     xAccel = accelerometer.get_x()
     yAccel = accelerometer.get_y()
     
-    x = 1;
-    y = 1;
+    sleep(100)
+
+
+    #250 seems like a decent sensitivity, button controlls are easier though
     
-    while 1 == 1:
-        if xAccel > 20:
-            x = x + 1
+    if xAccel > 300:
+        if x < 4:
+            x += 1
     
-        elif xAccel < -20:
-            x = x - 1
-        
-        
-        display.set_pixel(x, y, 9)
+    elif xAccel < -300:
+        if x > 0:
+            x -= 1
+            
+            
+
+    #Button controls
+    if button_a.is_pressed():
+        #if a is pressed, and the x isn't offscreen, move left
+        if x > 0:
+            x -= 1
+
+    elif button_b.is_pressed():
+        #vice versa
+        if x < 4:
+            x += 1
     
-    sleep(2000)
+    display.show(clear)
+    display.set_pixel(x, y, 9)
+    
+    
